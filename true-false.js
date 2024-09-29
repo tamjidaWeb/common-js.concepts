caa41122a93c2dc21af6c6627129efa7654eb780
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 12:31:04 +0600

    commited

--- true-false.js
+++ true-false.js
@@ -1,17 +1,38 @@
 /*Truthy:
 1.true
 2.any number positive or negative will be truthy except 0
+3.any string is truthy
+3. any string other than empty string [jodi space dya hoy ekta empty string tahole truthy hobe]
+4. '0','false' will be truthy
+5. empty object, empty array
 
 
 falsy:
 1.false
+2. 0 will be falsy
+3. empty string is falsy
+4. undefined is falsy
+5. null
 
 */
-const x = 0;
+let x =[];
+console.log(x)
 if(x){
-    console.log('value of x is truthy');
+    // console.log('value of x is truthy');
 
 }
 else{
-    console.log('value of x is falsy');
+    // console.log('value of x is falsy');
+}
+
+//optional
+//check falsy
+const y = '';
+if(!y){
+    // console.log('value is falsy');
+}
+const z = 5;
+//check truth
+if(!!z){
+    console.log('value is truthy')
 }
