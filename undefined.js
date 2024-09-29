3930e0c2f884eccb7c032b213a1f4db6b0bf9a42
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 11:53:01 +0600

    commited

---
+++ undefined.js
@@ -0,0 +1,13 @@
+// 8 ways to get undefine
+
+// let first;
+
+// function second(a,b){
+//     const total =a+b;
+// } 
+// const result = second();
+function third (a,b,c,d){
+    const total = a+b+c+d;
+}
+third(2,5)
+// console.log(result)
