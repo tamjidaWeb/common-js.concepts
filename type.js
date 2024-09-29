99bb8222d7f48cbb7d658f29572abbe7ce12b618
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 11:58:50 +0600

    commited

---
+++ type.js
@@ -0,0 +1,18 @@
+//strongly typed language
+// int a =5;
+//string b = 'alim halim khalim;
+//bool c = true
+
+
+//javascript is a dynamic type programming language
+
+//primitive type
+const a = 5;
+const b = 'samsu kopa';
+const d = true;
+
+//non-primitive
+const ages = [45,34,29];
+const student = {id:23, class:7};
+console.log(typeof a, typeof b, typeof ages, typeof d,typeof student);
+
