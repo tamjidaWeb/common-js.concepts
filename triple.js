ff97c8ecb9742d16ccb822496b60ca1912dac137
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 13:57:46 +0600

    commited

---
+++ triple.js
@@ -0,0 +1,16 @@
+//always use triple equal ===
+//equal comparisn doesnot work for non-primitive
+
+const first = [];
+const second = first;
+if(first == second){
+    console.log('values are equal');
+
+}
+else{
+    console.log('not equal');
+}
+
+//type coercion
+//type conversion
+//type casting
