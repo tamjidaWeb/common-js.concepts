db4d7aa58a07887a374715a648bdd3b8bc9a6a99
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 12:07:51 +0600

    commited

--- type.js
+++ type.js
@@ -14,5 +14,15 @@ const d = true;
 //non-primitive
 const ages = [45,34,29];
 const student = {id:23, class:7};
-console.log(typeof a, typeof b, typeof ages, typeof d,typeof student);
+// console.log(typeof a, typeof b, typeof ages, typeof d,typeof student);
+let x = 5;
+let y = x;
+console.log(x,y);
+y=7;
+console.log(x,y);
 
+const p = {job: 'web developer'};
+let q = p;
+console.log(p, q);
+q.job = 'front end developer';
+console.log(p, q)
