e12b68f0e27479bfc14140e645f6c94b77f24b46
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-29 12:13:50 +0600

    commited

---
+++ true-false.js
@@ -0,0 +1,17 @@
+/*Truthy:
+1.true
+2.any number positive or negative will be truthy except 0
+
+
+falsy:
+1.false
+
+*/
+const x = 0;
+if(x){
+    console.log('value of x is truthy');
+
+}
+else{
+    console.log('value of x is falsy');
+}
