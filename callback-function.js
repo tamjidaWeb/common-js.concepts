671073264535e8676ef4b60b8e02f219a1756196
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-01 14:33:52 +0600

    commited

--- callback-function.js
+++ callback-function.js
@@ -1,28 +1,36 @@
-function greeting(greetingHandler, name){
-    greetingHandler(name);
-}
-// const number = [45,42,87];
-// const laptop = {price: 4500, brand: 'lenevo', memory: '8gb'}
+// function greeting(greetingHandler, name){
+//     greetingHandler(name);
+// }
+// // const number = [45,42,87];
+// // const laptop = {price: 4500, brand: 'lenevo', memory: '8gb'}
 
-function greetingHandler (name){
-    console.log('good morning', name)
-}
+// function greetingHandler (name){
+//     console.log('good morning', name)
+// }
 
-function greetEvening(name){
-    console.log('good evening', name)
-}
+// function greetEvening(name){
+//     console.log('good evening', name)
+// }
 
-function greetNight(name){
-    console.log('good night', name)
+// function greetNight(name){
+//     console.log('good night', name)
 
+// }
+// greeting(greetingHandler, 'tom hacks');
+// greeting(greetingHandler, ' tom brakey')
+// greeting(greetingHandler, ' tom cruise')
+// greeting(greetEvening, ' tom solaiman')
+// greeting(greetEvening, ' tom salman')
+// greeting(greetNight, ' tom sallu');
+// function submitHandler(){
+//     console.log('submit button clicked')
+// }
+// document.getElementById('btn-submit').addEventListener('click',submitHandler)
+
+function A(){
+   
+    x=5;
+    console.log(x);
+    let x ;
 }
-greeting(greetingHandler, 'tom hacks');
-greeting(greetingHandler, ' tom brakey')
-greeting(greetingHandler, ' tom cruise')
-greeting(greetEvening, ' tom solaiman')
-greeting(greetEvening, ' tom salman')
-greeting(greetNight, ' tom sallu');
-function submitHandler(){
-    console.log('submit button clicked')
-}
-document.getElementById('btn-submit').addEventListener('click',submitHandler)
+A();
