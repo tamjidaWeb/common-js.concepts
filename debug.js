7e74f3615e3ffba90705ff20676e067d7b34b45f
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-02-12 09:29:24 +0600

    c

---
+++ debug.js
@@ -0,0 +1,111 @@
+// const number = [12, 4,56, 3,46,98];
+// console.log(number);
+// for (const num of number){
+//     console.log(num);
+    
+// }
+
+// let sum =0;
+// for(let i =11; i<=20; i++){
+   
+//     sum = sum + i;
+    
+// }
+// console.log("sum:",sum);  
+
+// for(let i= 20; i>=11; i--){
+//     console.log(i);
+// }
+
+// let n=10;
+// while(n>=0){
+//     console.log(n);
+//     n--;
+// }
+
+
+// for(let i =0; i<10;i=i+1){
+  
+//         console.log(i);
+//     }
+
+
+    // for (let i =1; i<=30; i++){
+    //     if(i%5===0 && i%3===0){
+    //         console.log(i);
+            
+    //     }
+    // }
+
+
+
+// let total =0;
+//     for (i=0; i<=10;i++){
+//         if(i%3===0){
+//             // console.log(i);
+//             total=total +i;
+//         }   
+//     }
+//     console.log(total);
+    
+
+//     for(let i=0; i<15; i++){
+//         console.log(i);
+//         if(i>=5){
+//             break;
+//         }
+        
+//     }
+//     console.log("life after breakup");
+
+//     let n = 54;
+//     while(n>25){
+//         console.log(n);
+//         if(n<51){
+//             break;
+//         }
+//         n--;
+        
+//     }
+    
+let odd = 1;
+let sum =0;
+for(let i =1;i<=5;i++){
+    if(i%2 === 1){
+        sum = sum + i;
+      
+        
+        
+    }
+  
+}
+// console.log(sum);
+
+let i =200;
+while(i>=100){
+    console.log(i);
+    i--;
+    break;
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
