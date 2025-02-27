f4782efb59277dd94c061bef165b0b77d2c9f95a
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-02-27 12:59:42 +0600

    c

--- debug.js
+++ debug.js
@@ -914,9 +914,19 @@ const numq =[12,56,33,65];
 
 
 
+// for(let i=0;i<10;i++){
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
 
-
-
+// let i=1;
+// while(i<50){
+//     if(i%3==0 && i%5==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
 
 
 
