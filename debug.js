270526603e01e78fb95e726c5d1f85e95e0a93c0
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-03-10 09:57:58 +0600

    c

--- debug.js
+++ debug.js
@@ -1157,8 +1157,20 @@ const numq =[12,56,33,65];
 
 
 
-
-
+// function evenNumbersOnly(numbers){
+//     let evens= [];
+//     for(const number of numbers){
+//         if(number%2==0){
+//             console.log(number);
+//             evens.push(number);
+          
+//         }
+//     }
+//     return evens;
+// }
+// const numbers = [2,3,4,5,6,7,8,9,10];
+// const evens =evenNumbersOnly(numbers);
+// console.log(evens)
 
 
 
