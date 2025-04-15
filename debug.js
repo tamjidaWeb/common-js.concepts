b3f2851f52ac03f3de4a5e84b3a55cb7141caf8e
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-15 21:45:10 +0600

    c

--- debug.js
+++ debug.js
@@ -2057,26 +2057,67 @@
 
 
 
-function discountedPrice(quantity){
+// function discountedPrice(quantity){
+//     if(quantity <=100){
+//         const total = quantity *100;
+//         return total;
+//     }
+
+//     else if(quantity >100 && quantity <=200){
+//         const total = quantity * 90;
+//         return total;
+//     }
+//     else{
+//         const total = quantity *70;
+//         return total;
+//     }
+// }
+
+// const quantityPrice = discountedPrice(150);
+// console.log(quantityPrice);
+
+//first 100 = 100 tk
+//101 to 200 = 90 tk
+//above 200 = 70
+
+
+
+function layeredDiscountedTotal(quantity){
+    const first100Price = 100;
+    const second100Price = 90;
+    const above200Price = 70;
+
     if(quantity <=100){
-        const total = quantity *100;
+        const total = first100Price * quantity;
         return total;
+    }
+    else if(quantity>100 && quantity<=200){
+
+        const first100Total = 100 * first100Price;
+        const second100Total = quantity - 100;
+        const remainingTotal =  second100Total * 90;
+        const total = first100Total + remainingTotal;
+        return total;
+
     }
 
-    else if(quantity >100 && quantity <=200){
-        const total = quantity * 90;
-        return total;
-    }
+
     else{
-        const total = quantity *70;
+        const first100 = 100 * first100Price;
+        const second200 = 100 * second100Price;
+        const above200 = quantity -200;
+        const above200Remaining = above200 * above200Price;
+        const total = first100 + second200 + above200Remaining;
         return total;
+       
+
     }
+
+
 }
 
-const quantityPrice = discountedPrice(150);
-console.log(quantityPrice);
-
-
+const total = layeredDiscountedTotal(2001);
+console.log(total)
 
 
 
