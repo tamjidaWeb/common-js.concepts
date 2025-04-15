4beaf8ae686381a7eaad18e336fe5061a668e76c
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-15 23:36:35 +0600

    c

--- debug.js
+++ debug.js
@@ -2140,8 +2140,43 @@
 
 
 
-function calculator(a , b , operation){
-    if(operation === ' add'){
-        return add(a,b);
-    }
+// function calculator(a , b , operation){
+//     if(operation === ' add'){
+//         return add(a,b);
+//     }
+
+//     else if(operation === ' substruct'){
+//         return substruct(a,b);
+//     }
+
+//     else if(operation === ' multiply'){
+//         return multiply(a,b);
+//     }
+
+//     else if(operation === 'divide'){
+//         return divide(a,b);
+//     }
+// }
+
+// const result = calculator(5,6, 'add');
+// console.log(result)
+
+
+
+
+function multiply(num1, num2){
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
