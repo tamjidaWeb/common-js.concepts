220fc29eede7ec1574d1aaf400f39ed19a2a713c
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-22 19:00:35 +0600

    c

--- dom.js
+++ dom.js
@@ -1,2 +1,13 @@
-console.log('hello')
-console.log(document)
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+section.innerHTML = `
+    <h1>Welcome to the DOM Manipulation Example</h1>
+    <p>Added a new paragraph for testing</p>
+
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+`;
+main.appendChild(section)
